e47f016ea4038d6e2ac9151b93de45be640cd00e
mddemarie <mddemarie@gmail.com>
2018-08-14 23:16:45 +0200

    learnt about mutations

---
+++ mutations.py
@@ -0,0 +1,25 @@
+import graphene
+
+class CreatePerson(graphene.Mutation):
+    class Arguments:
+        name = graphene.String()
+
+    ok = graphene.Boolean()
+    person = graphene.Field(lambda: Person)
+
+    def mutate(self, info, name):
+        person = Person(name=name)
+        ok = True
+        return CreatePerson(person=person, ok=ok)
+
+class Person(graphene.ObjectType):
+    name = graphene.String()
+    age = graphene.Int()
+
+class MyMutations(graphene.ObjectType):
+    create_person = CreatePerson.Field()
+
+class Query(graphene.ObjectType):
+    person = graphene.Field(Person)
+
+schema = graphene.Schema(query=Query, mutation=MyMutations)

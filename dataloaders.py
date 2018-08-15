43c8c42791c8664ed91e0b9c5dad214749186cf0
mddemarie <mddemarie@gmail.com>
2018-08-15 15:51:13 +0200

    learnt about dataloaders and middlewares

---
+++ dataloaders.py
@@ -0,0 +1,25 @@
+from promise import Promise
+from promise.dataloader import DataLoader
+
+import graphene
+
+class UserLoader(DataLoader):
+    def batch_load_fn(self, keys):
+        # Here I return a promise that will result on the
+        # corresponding user for each key in keys
+        return Promise.resolve([get_user(id=key) for key in keys])
+
+user_loader = UserLoader()
+user_loader.load(1).then(lambda user: user_loader.load(user.best_friend_id))
+user_loader.load(2).then(lambda user: user_loader.load(user.best_friend_id))
+
+class User(graphene.ObjectType):
+    name = graphene.String()
+    best_friend = graphene.Field(lambda: User)
+    friends = graphene.List(lambda: User)
+
+    def resolve_best_friend(self, info):
+        return user_loader.load(self.best_friend_id)
+
+    def resolve_friends(self, info):
+        return user_loader.load_many(self.friend_ids)
